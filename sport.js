c06eabe658489ea611575156aed4ec49dca661bf
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-24 00:10:47 +0630

    Create sport.js

---
+++ sport.js
@@ -0,0 +1,84 @@
+function l1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/line0.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/us-cnbc.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/z-winsport.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/us-golf.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/usa-usanetwork.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l6() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/es-tsn1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l7() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/es-tsn2.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l8() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/es-tsn3.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l9() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/es-tsn4.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l10() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/dmtn12.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l11() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/dmtn21.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l12() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/dmtn22.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l13() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://api.olelive.com/dis/live/CCTV5HD_360/live.flv?token=4500d89b2fd570d7da5b70f5b0c783ef|referer=https://www.olevod.com/player/live/tv-CCTV5HD-49.html/origin=https://www.olevod.com/player/live/tv-CCTV5HD-49.html#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function l14() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://api.olelive.com/dis/live/CCTV5PHD_360/live.flv?token=570b91b3fbd1591c8b78983be284574e|referer=https://www.olevod.com/player/live/tv-CCTV5PHD-50.html/origin=https://www.olevod.com/player/live/tv-CCTV5PHD-50.html#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sporttv1() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/pt-sporttv1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sporttv2() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/pt-sporttv2.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sporttv3() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/pt-sporttv3.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function lfctv() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/uk-lfctv.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function ufctv() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/ent-hbo2.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function bundesliga() {
+
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/linec9.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sportlaliga() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/10/playlist.m3u8|referer=https://95tv.live/watch-channel/super-sport-laliga-QvCmLA4IR.htm/origin=https://95tv.live/watch-channel/super-sport-laliga-QvCmLA4IR.htm/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function test2() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://streammain.myanmartvchannels.com:999/website-fortune-stream/website-fortune-stream/playlist.m3u8?|referer=https://www.myanmartvchannels.com/origin=https://www.myanmartvchannels.com/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function test3() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/25/playlist.m3u8|referer=https://live.95tv.live/origin=https://live.95tv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function test4() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://https://tv-lovat-six.vercel.app/api/tplay.php?mpd=63|referer=http://mrsmarttv.shop/origin=http://mrsmarttv.shop/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.android.chrome;S.browser_fallback_url=market://details?id=com.android.chrome;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+  
+
+
+
