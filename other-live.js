99da601491655392f5d51f6056e3c49657482eb2
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-24 00:08:27 +0630

    Create other-live.js

---
+++ other-live.js
@@ -0,0 +1,12 @@
+function truebein1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dooball2you.com/live/beinsport1/#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truebein3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dooball2you.com/live/beinsport3/#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function skysportepl() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/6/playlist.m3u8|Referer=http://live.95tv/|Origin=http://live.95tv/|&User-Agent=Mozilla/5.0 (Linux; Android 11; SM-A307GN Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/123.0.6312.121 Mobile Safari/537.36#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function skynetsport() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=89782#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
