042307996379383204175de97d607f88173a89e3
mmgold <77553706+naylinn2021@users.noreply.github.com>
2026-02-16 13:02:41 +0630

    Create get.php

---
+++ get.php
@@ -0,0 +1,107 @@
+#EXTM3U
+#EXTINF:-1,[MOVIE MYANMAR ACTION]
+http://203.81.84.130/hls/mwd_document/index.m3u8
+#EXTINF:-1,[MAHAR]
+http://tv.mahar.live/mahar/website.stream/chunks.m3u8
+#EXTINF:-1,[MWD FAMILY]
+http://203.81.84.130/hls/mwd_family/index.m3u8
+#EXTINF:-1,[MWD HD]
+http://203.81.84.130/hls/mwd_movie/index.m3u8
+#EXTINF:-1,[FORATUNETV HD]
+http://103.215.194.93:8282/hls/fortunetv/live/vmix_1080.m3u8
+#EXTINF:-1,[MWD MOVIE ]
+http://203.81.84.130/hls/mwd_serie/index.m3u8
+#EXTINF:-1,[MRTV-4]
+https://cdn-live-pyone.secureswiftcontent.com/han/MRTV4/MRTV4/LiveOutput/manifest.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTIvMzAvMjAyNSA4OjU5OjI5IEFNJmhhc2hfdmFsdWU9QU51VDdHaE9rVjNkZFMwa3JBSFR3QT09JnZhbGlkbWludXRlcz0xMCZpZD0yNDAwOjg0ODA6NDEwMzo3MjMwOmI5YzE6ODMwMjo1NGI4OjZiODM=
+#EXTINF:-1,[CHANNEL7]
+https://cdn-live-pyone.secureswiftcontent.com/han/PyonePlay/Channel7/LiveOutput/manifest.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTIvMzAvMjAyNSA5OjA2OjExIEFNJmhhc2hfdmFsdWU9WFJGUUxtZEJsenpGZHdvY2hiaWdmZz09JnZhbGlkbWludXRlcz0xMCZpZD0yNDAwOjg0ODA6NDEwMzo3MjMwOmI5YzE6ODMwMjo1NGI4OjZiODM=
+#EXTINF:-1,[ MITV ]
+https://mrtvott.com/cache/MITV/m3u8/hls_m3u8_MITV-TS-H.m3u8
+#EXTINF:-1,[MRTV HD]
+https://mrtvott.com/cache/MRTV-HD/m3u8/hls_m3u8_MRTV-HD-H.m3u8
+#EXTINF:-1,TEN SPORT FA CUP
+http://filex.me:8080/live/MAS101A/MAS101AABB/247171.ts
+#EXTINF:-1,ASTRO SPORT FA CUP
+http://filex.me:8080/live/MAS101A/MAS101AABB/290354.ts
+#EXTINF:-1,SONY SPORT FA CUP
+http://filex.me:8080/live/MAS101A/MAS101AABB/247156.ts
+#EXTINF:-1,Espn SPORT FA CUP 
+http://live.detodotvplay.com:8080/live/RaymundoBravo/gTYeNkcbEUcy/63392.ts
+#EXTINF:-1,VIP MONOMAX EPL 1
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1776175.ts
+#EXTINF:-1,VIP MONOMAX EPL 2
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1776176.ts
+#EXTINF:-1,VIP CANAL+SPORT EPL  
+http://185.160.192.64:80/live/mangin15/85784/139441.ts
+#EXTINF:-1,VIP ESPN PREMIERLAGUE 
+http://live.detodotvplay.com:8080/live/RaymundoBravo/gTYeNkcbEUcy/63392.ts
+#EXTINF:-1,VIP ALWAL EPL HD
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1641619.ts
+#EXTINF:-1,STAR SPORT SELECT 1
+http://filex.me:8080/live/MAS101A/MAS101AABB/247161.ts
+#EXTINF:-1,STAR SPORT SELECT 2
+http://filex.me:8080/live/MAS101A/MAS101AABB/247162.ts
+#EXTINF:-1,TNT SPORTS 1
+http://filex.me:8080/live/MAS101A/MAS101AABB/495258.ts
+#EXTINF:-1,VIP TRUE BEIN 3
+http://4kplus.info/ynskh24_847076/GyPAkfXo/1776212
+#EXTINF:-1,VIP SKY SPORT EFL
+http://nvkeusui.ottproxy.xyz:80/live/BAS4QU8B/KW5D2ZTC/29019.ts
+#EXTINF:-1,VIP POST SPORTS EPL
+http://atlan2025.me/live/Rochdi70sam/d3hm7lsqrh/1630676.ts
+#EXTINF:-1,VIP POST SPORT DUBYE
+http://atlan2025.me/live/Rochdi70sam/d3hm7lsqrh/1630675.ts
+#EXTINF:-1,VIP THMANY SPORT CR7
+http://atlan2025.me/live/Rochdi70sam/d3hm7lsqrh/1848331.ts
+#EXTINF:-1,VIP CANAL+360
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/12885.ts
+#EXTINF:-1,VIP SKYSPORT EPL 1
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/205919.ts
+#EXTINF:-1,VIP SKY FOOTBALL 
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/205924.ts
+#EXTINF:-1,VIP SKY MINI EPL
+http://atlan2025.me/live/Rochdi70sam/d3hm7lsqrh/205922.ts
+#EXTINF:-1,VIP TIVIBU SPORT
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/54287.ts
+#EXTINF:-1,VIP TV2 SPORTS 
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/263070.ts
+#EXTINF:-1,BelN PREMIERLAGUE 1
+http://atlan2025.me/Rochdi70sam/d3hm7lsqrh/510909
+#EXTINF:-1,VIP TRUE FOOTBALL 3
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1775968.ts
+#EXTINF:-1,VIP TRUE SPTV2
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1776069.ts
+#EXTINF:-1,VIP CANAL+EXTRA 2
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1108266.ts
+#EXTINF:-1,VIP SKY LIGIA 
+http://t77ert.top:80/live/d0:d0:03:36:33:7e/eqzw0eux/237533.ts
+#EXTINF:-1,VIP ASTRO EPL
+http://maveniptv.net:8080/live/Galal2025/11223344/322469.ts
+#EXTINF:-1,VIP YES SPORT
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/39981.ts
+#EXTINF:-1,(STAR SPORT2)THAILAND
+http://filex.me:8080/live/MAS101A/MAS101AABB/501230.ts
+#EXTINF:-1,CANAL+SPORT 1
+http://psk-sdk.ddno.us:8080/live/Etiziabd8266/scbdA0tracVTXOb/232526.ts
+#EXTINF:-1,CANAL+SPORT 2
+http://psk-sdk.ddno.us:8080/live/Etiziabd8266/scbdA0tracVTXOb/232527.ts
+#EXTINF:-1,CANAL+SPORT 4
+http://psk-sdk.ddno.us:8080/live/Etiziabd8266/scbdA0tracVTXOb/232529.ts
+#EXTINF:-1,CANAL+SPORT 5
+http://psk-sdk.ddno.us:8080/live/Etiziabd8266/scbdA0tracVTXOb/232530.ts
+#EXTINF:-1,BEIN VIP HD 1
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492949.ts
+#EXTINF:-1,BEIN VIP HD 2
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492953.ts
+#EXTINF:-1,BEIN VIP HD 3
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492957.ts
+#EXTINF:-1,BEIN VIP HD 4
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492961.ts
+#EXTINF:-1,BEIN VIP HD 5
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492965.ts
+#EXTINF:-1,BEIN VIP HD 6
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/492969.ts
+#EXTINF:-1,ALWAN SPORT HD 1
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/1641618.ts
+#EXTINF:-1,ALWAN SPORT HD 2
+http://azar2025.me:80/live/aj42u3bxph/bknb1fbw4k/164
